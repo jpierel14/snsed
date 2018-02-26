aa8724232c9e77b0ee2a6d8f15e7e5d27828fa73
Justin Roberts-Pierel <jr23@email.sc.edu>
2018-02-26 12:43:52 -0500

    updated requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='snsedextend',
+	install_requires=['sncosmo','astropy'],
+	author='J. R. Pierel',
+	author_email='jr23@email.sc.edu'
+	version='0.0.1'
+)
