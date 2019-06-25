a2af10d4bb7de56ab56229cc21134b7b2d3de451
Justin Roberts-Pierel <jr23@email.sc.edu>
2019-06-25 11:59:32 -0400

    fixed untracked files

--- bianco/moveFiles.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-import sncosmo
-
-lcs=sncosmo.read_lc('../lc.standardsystem.sesn_allphot.dat')
-sne=np.unique(lcs['Name'])
-for s in sne:
-	sncosmo.write_lc(lcs[lcs['Name']==s],'lc_'+s+'.dat')
